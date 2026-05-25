04b7da7d202fbade36ec20364f71eccc40a24541
Oscar Chavez Villada <chavezoscar311@gmail.com>
2026-05-24 21:31:58 -0600

    correcion de correo 4

--- config/mail.php
+++ config/mail.php
@@ -8,10 +8,10 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
+            'scheme' => env('MAIL_SCHEME', 'smtp'),
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'port' => env('MAIL_PORT', 587),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => env('MAIL_TIMEOUT', 10),
@@ -63,8 +63,8 @@ return [
     ],
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'chavezoscar311@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'CEFIRET - Sistema de Rehabilitación'),
     ],
 
 ];
